e4583b0e139fda215fa39df9d1715c3b06490c86
Lyolya <almasilaura@outlook.com>
2021-06-17 19:21:43 +0100

    initial commit

---
+++ example.py
@@ -0,0 +1,24 @@
+
+
+# TODO
+
+# 1. install flask
+# 2. make app.py
+# 3. make console.py
+# 4. put boilerplate in app.py
+# 5. make home route inapp.py
+# 6. display hello world
+# 7. make a flask.env
+# 8. test runtime
+
+# 9. create database
+# 10. create tables
+# 11. create run/sql file
+# 11.b/ install psycopg2
+# 12. create model 
+# 13. create repository
+# 14. populate console.py
+# 15. create controller
+# 16. apply blueprint to app.py
+# 17. create base html
+# 18. create index html
